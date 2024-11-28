53d5f3cf2665a00bc972c27b8cdd2fd6b96d29d2
Hanz Jonathan Manuel <hanzjmanuel@gmail.com>
2024-11-28 23:47:03 +0800

    query all set

---
+++ application/Services/StudentService/Query/GetStudentQuery.cs
@@ -0,0 +1,25 @@
+﻿using domain.Interfaces;
+using domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application.Services.StudentService.Query
+{
+    public record GetStudentQuery(int id) : IRequest<Student>;
+    public class GetStudentQueryHandler : IRequestHandler<GetStudentQuery, Student>
+    {
+        private readonly IStudentRepository _repo;
+        public GetStudentQueryHandler(IStudentRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<Student> Handle(GetStudentQuery request, CancellationToken token)
+        {
+            return await _repo.Get(request.id);
+        }
+    }
+}
